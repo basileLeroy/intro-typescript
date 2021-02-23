6cdd377483a5af3c12651a7d8e5634a3794c5ba5
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-02-23 20:48:24 +0100

    Adding Webpack.config file and configurating the settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 module.exports = {
     entry: "./src/**/*",
+
     output: {
-    path: "/public",
-    filename: "script.js",
+        path: "/public",
+        filename: "script.js",
     },
+
     resolve: {
-    extensions: [".tsx", ".ts", ".js", ".json"],
+        extensions: [".tsx", ".ts", ".js", ".json"],
     },
+    
     module: {
-    rules: [
-        // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-        { test: /\.tsx?$/, use: ["ts-loader"], exclude: /node_modules/ },
-    ],
+        rules: [
+            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            { 
+                test: /\.tsx?$/, 
+                use: ["ts-loader"], 
+                exclude: /node_modules/ 
+            },
+        ],
     },
 };
